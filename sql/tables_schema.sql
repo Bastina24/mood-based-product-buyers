2f83a3d13261ccfcd8ee274293c6d77491a176a8
Backia Bastina.J <156083335+Bastina24@users.noreply.github.com>
2026-01-14 08:57:42 +0530

    Update tables_schema.sql                Added SQL table creation queries

--- sql/tables_schema.sql
+++ sql/tables_schema.sql
@@ -1,1 +1,43 @@
+create database myproject;
+use myproject;
+create table buyer(
+Buyer_ID int primary key,
+Buyer_Name varchar(15),
+Gender varchar(10),
+Age float,
+Occupation varchar(25),
+Marital_Status varchar(20),
+City varchar(20),
+State varchar(25)
+)
+select * from buyer;
+select * from buyer_details;
 
+
+create table feedback(
+Purchase_ID int primary key,
+Weather varchar(15),
+Rating float,
+Feedback varchar(15)
+)
+select * from feedback;
+select * from feedback_details;
+
+
+create table purchase(
+Purchase_ID int primary key,
+Buyer_ID int,
+Mood varchar(15),
+Emotion_Intensity int,
+Product_Category varchar(15),
+Product_Name varchar(20),
+Brand varchar(10),
+Amount int,
+Discount int,
+Payment_Mode varchar(30),
+Purchase_Date date,
+Purchase_Time varchar(30),
+Device_Used varchar(10)
+);
+select * from purchase;
+select * from purchase_details;
